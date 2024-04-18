b5d91b136b2706a647bef09f9b1d192f7b6a7a36
unknown <ye37seol@gmailcom>
2024-04-17 17:03:08 -0700

    edit render pw

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,31 +1,29 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const is_render = process.env.IS_RENDER || false;
 
 const dbConfigRender = {
-	host: "sql.freedb.tech",
-	user: "freedb_2350_main.",
-	password: "m?eg6bwvU8Qz8yz",
-	database: "freedb_COMP2350_A01342823",
-	multipleStatements: false,
-	namedPlaceholders: true
+  host: "sql.freedb.tech",
+  user: "freedb_2350_main.",
+  password: "&c!gQXSFgVweD*4",
+  database: "freedb_COMP2350_A01342823",
+  multipleStatements: false,
+  namedPlaceholders: true,
 };
 
 const dbConfigLocal = {
-	host: "localhost",
-	user: "root",
-	password: "abc123!",
-	database: "freedb_comp2350_a01342823",
-	multipleStatements: false,
-	namedPlaceholders: true
+  host: "localhost",
+  user: "root",
+  password: "abc123!",
+  database: "freedb_comp2350_a01342823",
+  multipleStatements: false,
+  namedPlaceholders: true,
 };
 
 if (is_render) {
-	var database = mysql.createPool(dbConfigRender);
-}
-else {
-	var database = mysql.createPool(dbConfigLocal);
+  var database = mysql.createPool(dbConfigRender);
+} else {
+  var database = mysql.createPool(dbConfigLocal);
 }
 
 module.exports = database;
-		
